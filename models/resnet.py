59abc74eecd04cd66d1f419613a70d53f8b33b56
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2021-07-28 21:01:28 +0300

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -121,6 +121,13 @@ class ResNet(nn.Module):
 
         self.avgpool = nn.AdaptiveAvgPool2d(output_size=1)
         self.fc = nn.Linear(in_features=2048, out_features=1000, bias=True)
+        
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
     
     def __add_blocks(self, out_filters, count, stride=1):
         """
