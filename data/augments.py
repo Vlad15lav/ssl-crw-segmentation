c2ce900ae5e9c6df52a936761c576b6abd02c5d9
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2021-08-12 20:14:07 +0300

    Update augments.py

--- data/augments.py
+++ data/augments.py
@@ -1,10 +1,14 @@
-import torchvision
 import skimage
-
 import torch
-from torchvision import transforms
-
+import torchvision
 import numpy as np
+
+from torchvision import transforms
 from PIL import Image
 
-# TODO
+def get_train_augmentation(opt):
+    resize_aug = transforms.Resize(opt.img_size)
+    norm_aug = [transforms.ToTensor(), 
+        transforms.Normalize(opt.mean_norm, opt.std_norm)]
+    
+    
