12dd7dbab36ed2cd6045b5ee8e2ff41b651b2b54
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2021-08-23 17:21:57 +0300

    Update trainCRW.py

--- trainCRW.py
+++ trainCRW.py
@@ -13,7 +13,7 @@ from IPython import display
 
 from data.datasets import Kinetics400
 from data.augments import get_train_augmentation
-from models.resnet import get_resnet
+from models.сrw import CRW
 from utils.util import collate_fn, get_sheduler, adjust_learning_rate
 
 def get_args():
@@ -22,10 +22,19 @@ def get_args():
     parser.add_argument('--weight-path', type=str, default='state/crw', help='path for logs, weights training')
 
     parser.add_argument('--depth', type=int, default=18, help='depth resnet model')
+    parser.add_argument('--head-depth', type=int, default=0, help='depth head')
     parser.add_argument('--pretrained', help='load imagenet weights', action="store_true")
     parser.add_argument('--cont-train', help='use last weights', action="store_true")
+    parser.add_argument('--temperature', type=float, default=0.05, help='(temperature) shaping')
+    parser.add_argument('--featdrop', type=float, default=0.1, help='dropout rate on maps)
+    parser.add_argument('--edgedrop', type=float, default=0.0, help=' dropout rate on A')
 
     parser.add_argument('--img-size', nargs='+', type=int, default=[256, 256], help='image size')
+    parser.add_argument('--clip-len', default=4, type=int, metavar='N',
+                        help='number of frames per clip')
+    # parser.add_argument('--clips-per-video', default=5, type=int, metavar='N',
+    #                     help='maximum number of clips per video to consider')
+    parser.add_argument('--frame-skip', default=8, type=int, help='kinetics: fps | others: skip between frames')
     parser.add_argument('--augs', type=str, default='crop', help='select augmentation (crop, jitter, flip, grid)')
     parser.add_argument('--patch-size', nargs='+', type=int, default=[64, 64], help='patch size')
     parser.add_argument('--mean-norm', nargs='+', type=int, default=[0.4914, 0.4822, 0.4465], help='mean pixel')
@@ -100,8 +109,8 @@ if __name__ == '__main__':
     # load dataloaders
     transform_train = get_train_augmentation(opt)
 
-    trainset = Kinetics400(root=opt.data_path + '/train', frames_per_clip=8, step_between_clips=1,
-                            frame_rate=8, transform=transform_train)
+    trainset = Kinetics400(root=opt.data_path + '/train', frames_per_clip=opt.clip_len, step_between_clips=1,
+                            frame_rate=opt.frame_skip, transform=transform_train)
     # validset = Kinetics400(root=opt.data_path + '/valid', frames_per_clip=8, step_between_clips=1,
     #                         frame_rate=8)#, transform=transform_train)
     train_sampler = RandomSampler(trainset)
@@ -111,23 +120,17 @@ if __name__ == '__main__':
     #                 num_workers=opt.n_work, pin_memory=True, collate_fn=collate_fn)
 
     # TODO: load model
-    # model = get_resnet(opt.depth, opt.pretrained)
+    model = CRW(opt).to(opt.device)
     # model.load_state_dict(torch.load(f'{opt.weight_path}/last_weights.pth'))
 
     # optimizer and sheduler
     lr_schedule = get_sheduler(opt.lr, opt.final_lr, len(trainloader), opt.epoches, opt.warm_up, opt.wup_lr)
 
-#     if opt.adam:
-#         optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr,
-#             weight_decay=opt.wd)
-#     else:
-#         optimizer = torch.optim.SGD(model.parameters(), lr=opt.lr,
-#             momentum=cfg.momentum, weight_decay=opt.wd)
+    if opt.adam:
+        optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr,
+            weight_decay=opt.wd)
+    else:
+        optimizer = torch.optim.SGD(model.parameters(), lr=opt.lr,
+            momentum=cfg.momentum, weight_decay=opt.wd)
     
-    for video in trainloader:
-        break
-    
-    print(video)
-    print(len(video))
-
     #train(model, trainloader, validloader, optimizer, lr_schedule, opt)
