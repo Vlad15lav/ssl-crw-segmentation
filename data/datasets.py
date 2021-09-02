4657e5956bab3ea318fdf43d641c97b1ff4b9a5c
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2021-09-03 00:19:26 +0300

    Update datasets.py

--- data/datasets.py
+++ data/datasets.py
@@ -7,6 +7,7 @@ from torchvision.datasets.utils import list_dir
 from torchvision.datasets.folder import make_dataset
 from torchvision.datasets.vision import VisionDataset
 
+from utils.tools import *
 
 class Kinetics400(VisionDataset):
     """
@@ -59,3 +60,97 @@ class Kinetics400(VisionDataset):
             video = self.transform(video)
 
         return video, audio, label
+
+class DAVIS(torch.utils.data.Dataset):
+    """
+    https://davischallenge.org/
+    """
+    def __init__(self, opt):
+        self.path = opt.data_path
+        self.img_size = opt.img_size
+        self.video_len = opt.video_len
+        
+        self.images_path = os.path.join(self.path, 'JPEGImages', '480p')
+        self.masks_path = os.path.join(self.path, 'Annotations', '480p')
+        self.classes_list = os.listdir(self.images_path)
+
+        self.mean, self.std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
+
+    def __len__(self):
+        return len(self.classes_list)
+
+    def num_classes(self):
+        return len(self.classes_list)
+
+    def label_to_name(self, num_class):
+        return self.classes_list[num_class]
+
+    def get_paths(self, folder_imgs, folder_masks):
+        img_files, mask_files = os.listdir(folder_imgs), os.listdir(folder_masks)
+
+        frame_num = len(img_files) + self.video_len
+        img_files.sort(key=lambda x:int(x.split('.')[0]))
+        mask_files.sort(key=lambda x:int(x.split('.')[0]))
+
+        imgs_paths, masks_paths = [], []
+
+        for i in range(frame_num):
+            i = max(0, i - self.video_len)
+            img_path = os.path.join(folder_imgs, img_files[i])
+            mask_path = os.path.join(folder_masks,  mask_files[i])
+
+            imgs_paths.append(img_path)
+            masks_paths.append(mask_path)
+
+        return imgs_paths, masks_paths
+
+    def load_image(self, path):
+        img = cv2.imread(path)
+        img = img.astype(np.float32) / 255.
+        img = img[:, :, ::-1]
+        img = img.copy()
+        img = np.transpose(img, (2, 0, 1)) # CxHxW
+        img = torch.from_numpy(img).float()
+        return img
+
+    def __getitem__(self, index):
+        class_imgs = os.path.join(self.images_path, self.classes_list[index])
+        class_masks = os.path.join(self.masks_path, self.classes_list[index])
+
+        frame_num = len(os.listdir(class_imgs)) + self.video_len
+        imgs_paths, masks_paths = self.get_paths(class_imgs, class_masks)
+
+        imgs = []
+        imgs_orig = []
+        masks = []
+
+        for i in range(frame_num):
+            img_path, mask_path = imgs_paths[i], masks_paths[i]
+            img = self.load_image(img_path)
+            mask = cv2.imread(mask_path)
+
+            # resize image and mask
+            img = resize(img, self.img_size)
+            mask = cv2.resize(mask, self.img_size, cv2.INTER_NEAREST)
+
+            img_orig = img.clone()
+
+            # normalize pixels
+            img = color_normalize(img, self.mean, self.std)
+
+            imgs.append(img)
+            imgs_orig.append(img_orig)
+            masks.append(mask)
+        
+        imgs = torch.stack(imgs)
+        imgs_orig = torch.stack(imgs_orig)
+        masks = np.stack(masks)
+
+        # mask set channel
+        mask_set = masks[0].copy().reshape(-1, masks.shape[-1]).astype(np.uint8)
+        mask_set = np.unique(mask_set, axis=0)
+
+        if np.all((mask_set[1:] - mask_set[:-1]) == 1):
+            mask_set = mask_set[:, :1]
+
+        return imgs, imgs_orig, masks, mask_set
